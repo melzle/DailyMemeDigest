711c0f605823a928000b3c60b1bcf616df6adc9b
Christopher Evan <christopher.evan0603@gmail.com>
2023-01-06 22:07:01 +0700

    e

--- DailyMemeDigest/app/src/main/java/com/sukacita/dailymemedigest/LoginActivity.kt
+++ DailyMemeDigest/app/src/main/java/com/sukacita/dailymemedigest/LoginActivity.kt
@@ -2,10 +2,20 @@ package com.sukacita.dailymemedigest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnSignIn_LoginActivity.setOnClickListener() {
+            val username = txtUsername.text
+            val password = txtPassword.text
+
+            val q = Volley.
+        }
     }
+
 }
