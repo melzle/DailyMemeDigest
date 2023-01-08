a92a3458445e9ae8f10a71eed78734637e6b8682
Meliyana <meliyana128@gmail.com>
2023-01-08 20:44:30 +0700

    ui update

---
+++ DailyMemeDigest/app/src/main/java/com/sukacita/dailymemedigest/DetailMemeActivity.kt
@@ -0,0 +1,11 @@
+package com.sukacita.dailymemedigest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailMemeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_meme)
+    }
+}
